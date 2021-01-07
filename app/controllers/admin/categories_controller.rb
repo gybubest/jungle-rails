d890beb65c106ab6d0e2bf7fd27b2029aec4c1f3
Yu Gao <gaoyububest@gmail.com>
2021-01-07 20:16:01 +0000

    Add categories page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,27 @@
 class Admin::CategoriesController < ApplicationController
-  
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+
   def index
+    @categories = Category.order(id: :asc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(Category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def Category_params
+    params.require(:Category).permit(:name)
   end
 end
