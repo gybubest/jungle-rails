5278db019d4c4f8ce6b22c174ce8f9f97be8d99c
Yu Gao <gaoyububest@gmail.com>
2021-01-11 22:21:54 +0000

    Add validation for user signup name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
-  validates :email, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+
   has_secure_password
 end
